f5b2aaed28d9b74ea09821b6fc8d59c9518a608d
darab96 <106982371+darab96@users.noreply.github.com>
2023-02-11 20:37:01 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
-/** @format */
-
 let arr = [
   { id: 1, name: "john", age: "18", marks: 80 },
   { id: 2, name: "jack", age: "20", marks: 85 },
   { id: 3, name: "karen", age: "19", marks: 35 },
 ];
 
-function PrintStudentswithMap() {
-  //Write your code here , just console.log
+function PrintStudentbyMap() {
+  arr.map((item) => {
+    console.log(item);
+  });
 }
 
 function PrintStudentsbyForEach() {
-  //Write your code here , just console.log
+  arr.forEach((item) => {
+    console.log(item);
+  });
 }
 
 function addData() {
-  //Write your code here, just console.log
+  let student = { id: 4, name: "susan", age: "20", marks: 45 };
+  arr.push(student);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
-  //Write your code here, just console.log
+  let arr1 = arr.filter((item) => item.marks >= 50);
+  console.log(arr1);
 }
 
-function concatenateArray() {
-  //Write your code here, just console.log
+function ConcatenateArray() {
+  let students = [{ id: 5, name: "james", age: "21", marks: 75 },
+  { id: 6, name: "jane", age: "22", marks: 65 },
+  { id: 7, name: "jim", age: "23", marks: 55 }];
+  let arr2 = arr.concat(students);
+  console.log(arr2);
 }
